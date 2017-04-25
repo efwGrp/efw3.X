01a610a02d43b7adc632ee98591df15ad2ce2619
Chang Kejun <changkejun@hotmail.com>
2017-04-25 10:32:43 +0900

    remove withoutJQuery attr from Client tag

--- source for jar/efw/taglib/Client.java
+++ source for jar/efw/taglib/Client.java
@@ -16,10 +16,6 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
  */
 public class Client extends SimpleTagSupport implements DynamicAttributes {
 	/**
-	 * efwのjsとcssのみにするかどうか
-	 */
-	private boolean withoutJQuery=false;	
-	/**
 	 * タグ処理。
 	 *efwの基本機能を利用するため、複数のcssとjsを取り込むタグを作成する。
 	 */
@@ -27,14 +23,12 @@ public class Client extends SimpleTagSupport implements DynamicAttributes {
 	public void doTag() throws JspException, IOException {
 		super.doTag();
 		JspWriter out = this.getJspContext().getOut();
-		if (!withoutJQuery){
-			out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/efw.css\">");
-			out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/jquery-ui.min.css\">");
-			out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/jquery-ui.structure.min.css\">");
-			out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/jquery-ui.theme.min.css\">");
-			out.print("<script type=\"text/javascript\" charset=\"UTF-8\" src=\"./efw/jquery-min.js\"></script>");
-			out.print("<script type=\"text/javascript\" charset=\"UTF-8\" src=\"./efw/jquery-ui.min.js\"></script>");
-		}
+		out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/efw.css\">");
+		out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/jquery-ui.min.css\">");
+		out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/jquery-ui.structure.min.css\">");
+		out.print("<link type=\"text/css\" rel=\"stylesheet\" href=\"./efw/jquery-ui.theme.min.css\">");
+		out.print("<script type=\"text/javascript\" charset=\"UTF-8\" src=\"./efw/jquery-min.js\"></script>");
+		out.print("<script type=\"text/javascript\" charset=\"UTF-8\" src=\"./efw/jquery-ui.min.js\"></script>");
 		out.print("<script type=\"text/javascript\" charset=\"UTF-8\" src=\"./efw/js.cookie.min.js\"></script>");
 		out.print("<script type=\"text/javascript\" charset=\"UTF-8\" src=\"./efw/efw.client.messages.js\"></script>");
 		out.print("<script type=\"text/javascript\" charset=\"UTF-8\" src=\"./efw/efw.client.format.js\"></script>");
@@ -50,14 +44,5 @@ public class Client extends SimpleTagSupport implements DynamicAttributes {
 	@Override
 	public void setDynamicAttribute(String uri, String name, Object value)
 			throws JspException {
-		if(name.equalsIgnoreCase("withoutjquery")){
-			if (((String)value).equalsIgnoreCase("true")){
-				withoutJQuery=true;
-			}
-		}else{
-			//attrの設定は不要です。削除する。
-			//this.getJspContext().setAttribute(name, value,PageContext.REQUEST_SCOPE);
-		}
-		
 	}
 }
